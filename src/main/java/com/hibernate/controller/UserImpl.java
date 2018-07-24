e9ba01c1a5ffb924e3784a3ea8a2f7aeb9c7ac40
Pravin P Prajapati <pravinp.prajapati@gmail.com>
2018-07-25 00:37:26 +0530

    Working copy of Crud Operation using Spring boot, rest, theme-leaf, hibernate.

--- src/main/java/com/hibernate/controller/UserImpl.java
+++ src/main/java/com/hibernate/controller/UserImpl.java
@@ -1,11 +1,17 @@
 package com.hibernate.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.hibernate.dao.UserDaoImpl;
 import com.hibernate.model.User;
@@ -19,6 +25,7 @@ public class UserImpl {
 	private UserDaoImpl userDao = null;
 	List<User> allUsers = null;
 	
+	// url : http://localhost:8080/HibernateMVCCrud/
 	@RequestMapping("/")
 	public String getAllUsers(Model model)
 	{
@@ -26,4 +33,88 @@ public class UserImpl {
 		model.addAttribute("allUsers", allUsers);
         return "manageUser";
 	}
+	
+	@RequestMapping("/{userId}")
+	public String getUserById(Model model, @PathVariable("userId") int userId)
+	{
+		System.out.println("User id="+userId);
+		User user = userDao.getUserById(userId);
+		model.addAttribute("allUsers", user);
+		return "manageUser";
+	}
+	
+	@RequestMapping("/deleteUser")
+	public String deleteUser(Model model,@RequestParam(value="id", required=false, defaultValue="0") int userId)
+	{
+		userDao.deleteUser(userId);
+		
+		allUsers = userDao.getAllUsers();
+		model.addAttribute("allUsers", allUsers);
+		return "manageUser";
+	}
+	
+	@GetMapping("/insertUser")
+	public String insertUserPre(Model model)
+	{
+		model.addAttribute("user", new User());
+		return "insertUser";
+	}
+
+	@PostMapping(value="/inserUserObject")
+	public String insertUser(@ModelAttribute User user, Model model,@RequestParam Map<String,String> paramMap /*, 
+			@RequestParam(value="userName") String userName1, @RequestParam(value="password") String password1, @RequestParam(value="userEmail") String email1*/)
+	{
+		//int id = Integer.parseInt(request.getParameter("id"));
+		String userName=paramMap.get("userName");
+		String password=paramMap.get("password");
+		String email=paramMap.get("email");
+
+		//User user = new User(userName,password,email);
+		userDao.insertUser(user);
+		
+		allUsers = userDao.getAllUsers();
+		model.addAttribute("allUsers", allUsers);
+		return "manageUser";
+	}
+	
+
+	@GetMapping("/updateUser")
+	public String updateUserPre(Model model,@RequestParam(value="id", required=false, defaultValue="0") int userId)
+	{
+		System.out.println("Pre user id = "+userId);
+		User user = userDao.getUserById(userId);
+		
+		model.addAttribute("user", user);
+		return "updateUser";
+	}
+	
+	@PostMapping(value="/updateUserObject")
+	public String updateUser(@ModelAttribute User user, Model model,@RequestParam Map<String,Object> paramMap /*, 
+			@RequestParam(value="userName") String userName1, @RequestParam(value="password") String password1, @RequestParam(value="userEmail") String email1*/)
+	{
+		
+		// Working 
+		//Integer id = (Integer) paramMap.get("userId");
+		String userName=(String) paramMap.get("userName");
+		String password=(String) paramMap.get("password");
+		String email=(String) paramMap.get("email");
+		
+		User updatedUser = userDao.getUserById(user.getUserId());
+		updatedUser.setUserName(userName);
+		updatedUser.setPassword(password);
+		updatedUser.setEmail(email);
+		
+		userDao.updateUser(updatedUser);
+		
+		
+		// Not working, do not know why
+		/*User updatedUser = userDao.getUserById(user.getUserId());
+		updatedUser = new User(user.getUserName(),user.getPassword(),user.getEmail()); 
+		userDao.updateUser(updatedUser);*/
+		
+		allUsers = userDao.getAllUsers();
+		model.addAttribute("allUsers", allUsers);
+		return "manageUser";
+	}
+	
 }
--- src/main/java/com/hibernate/dao/UserDaoImpl.java
+++ src/main/java/com/hibernate/dao/UserDaoImpl.java
@@ -4,10 +4,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,12 +24,58 @@ public class UserDaoImpl {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
-	
 	public List<User> getAllUsers()
 	{
-		//String hql = "select u.userId userId, u.userName userName, u.password password, u.email email from User u";
+		List<User> allUsers = new ArrayList<User>();
+		
+		// Way-1
 		String hql = "from User";
-		return (List<User>) entityManager.createQuery(hql).getResultList();
+		allUsers = (List<User>) entityManager.createQuery(hql).getResultList();
+		
+		// Way-2
+		/*String hql = "select userId, userName, password, email from User";
+		Query qry = entityManager.createQuery(hql);
+		List<Object[]> l =qry.getResultList();
+		for (Object[] row : l) {
+			System.out.println("User values : " + row[0] + row[1] + row[2] + row[3]);
+			User user = new User(Integer.parseInt(row[0].toString()), row[1].toString(), row[2].toString(),
+					row[3].toString());
+			System.out.println(user.getUserId() + user.getUserName() + user.getPassword() + user.getEmail());
+			allUsers.add(user);
+		}*/
+		
+		// Way-3
+		/*String hql = "select new map(userId, userName, password, email) from User";
+		Query qry = entityManager.createQuery(hql);
+		List<Object[]> l =qry.getResultList();
+		for (Object object : l) {
+			Map map = (Map) object;
+			User user = new User(Integer.parseInt(map.get("0").toString()), map.get("1").toString(),
+					map.get("2").toString(), map.get("3").toString());
+			System.out.println(user.getUserId() + user.getUserName() + user.getPassword() + user.getEmail());
+			allUsers.add(user);
+		}*/
+		
+		return allUsers;
+	}
+	
+	public User getUserById(int userId) {
+		return entityManager.find(User.class, userId);
+	}
+	
+	public void deleteUser(int userId) {
+		entityManager.remove(getUserById(userId));
+	}
+	
+	public void insertUser(User user)
+	{
+		entityManager.persist(user);
+	}
+	
+	public User updateUser(User user) {
+		entityManager.flush();
+	
+		return user;
 	}
 	
 }
